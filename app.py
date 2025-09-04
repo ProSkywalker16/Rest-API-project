f612e3942db29a45b4d1fad28b1de05462eb10d4
Promit Chaudhuri <134211908+ProSkywalker16@users.noreply.github.com>
2025-09-05 02:27:32 +0530

    New changes

--- app.py
+++ app.py
@@ -70,3 +70,7 @@ def get_item_in_store(name):
         if store["name"] == name:
             return {"items": store["items"]}
     return {"message": "Store not found"}, 404
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
